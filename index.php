afa5f334e45339de2ee6ff18fc33804de17e69d9
Arjan de Ruijter <rra@mboutrecht.nl>
2015-10-12 10:36:33 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>inlogregistratietutorialsite</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div id="container">
+			<div id="banner">
+				Banner
+			</div>
+			<div id="content">
+				Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site!Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! Dit wordt de container voor onze site! 
+			</div>
+			<div id="footer">
+				footer
+			</div>			
+		</div>
+	</body>
+</html>
