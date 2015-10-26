ff0df2c77b739dcd4747a764efa83ff2182373a8
Arjan de Ruijter <rra@mboutrecht.nl>
2015-10-26 09:55:12 +0100

    mail

--- register.php
+++ register.php
@@ -2,29 +2,39 @@
 	//var_dump($_POST);
 	include("db_connect.php");
 	
+	//var_dump($_POST);
+	
 	$query = "INSERT INTO `users` (`id`,
 								   `firstname`,
 								   `infix`,
 								   `lastname`,
 								   `email`) 
 			  VALUES 			  (NULL,
-								   'Arjan',
-								   'de',
-								   'Ruijter',
-								   'adruijter@gmail.com');";
-								   
+								   '".$_POST["firstname"]."',
+								   '".$_POST["infix"]."',
+								   '".$_POST["lastname"]."',
+								   '".$_POST["email"]."');";
+	//echo $query;exit();
+	
 	$result = mysqli_query($connection, $query);
+	
+	//var_dump($result);
 	
 	if ( $result )
 	{
 		// Succesmelding en een redirect naar homepage
+		$emailaddress = $_POST["email"];
+		$subject = "Activateer uw account voor inlogregistratietutorialsite";
+		$message = "Bedankt voor het registreren, klik op de volgende activatielink. http://localhost/2015-2016/blok1/am1a/inlogregistratietutorialsite/index.php?content=activatie";
+		mail($emailaddress, $subject, $message);
+		echo "Uw gegevens zijn correct door ons ontvangen. U ontvangt een bevestigings e-mail met daarin een activatielink. Voor het kunnen inloggen is het nodig dat u uw account op deze manier activeert. U wordt doorgestuurd naar de homepage.";
+		header("refresh:6000; url=index.php?content=homepage");
 	}
 	else
 	{
-		//foutmelding neem contact op met info@inlogregistratietutorialsite.nl redirect naar de homepage
+		echo "foutmelding. Neem contact op met info@inlogregistratietutorialsite.nl. Uw gegevens zijn niet door ons ontvangen. U wordt doorgestuurd naar de homepage.";
+		header("refresh:5; url=index.php?content=homepage");
 	}
-	
-	echo MD5("ArjandeRuijter");
 	
 	/* Huiswerk voor 26 oktober 8:45 afmaken
 	 * Zorg dat de formuliergegevens goed weggeschreven staan in de database
