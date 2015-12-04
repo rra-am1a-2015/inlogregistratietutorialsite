cb94126545eb482722592efaa6b663f5f2e9f8e3
Arjan de Ruijter <rra@mboutrecht.nl>
2015-12-04 11:25:23 +0100

    Jquery

--- tutorials/jquery/tut_jquery_selectors.php
+++ tutorials/jquery/tut_jquery_selectors.php
@@ -1,1 +1,33 @@
 <h5>JQuery Selectors</h5>
+Geef alle li in ul met id list1 een rode rand.
+Geef alle p tags een blauwe rand.
+Geef alle p tags na ul met id=list1 een paarse rand.
+
+
+<ul id="list1">
+	<li class="a">item 1</li>
+	<li class="a">item 2</li>
+	<li class="b">item 3</li>
+	<li class="b">item 4</li>
+</ul>
+<p>Dit is de eerste paragraaf</p>
+<p>Dit is de tweede paragraaf</p>
+<p>Dit is de derde paragraaf</p>
+<p>Dit is de vierde paragraaf</p>
+<p>Dit is de vijfde paragraaf</p>
+<ul id="list2">
+	<li class="a">item 1</li>
+	<li class="a">item 2</li>
+	<li class="b">item 3</li>
+	<li class="b">item 4</li>
+</ul>
+<p>Dit is de eerste paragraaf</p>
+<p>Dit is de tweede paragraaf</p>
+<p>Dit is de derde paragraaf</p>
+<p>Dit is de vierde paragraaf</p>
+<p>Dit is de vijfde paragraaf</p>
+<p>Dit is de zesde paragraaf</p>
+<p>Dit is de zevende paragraaf</p>
+<p>Dit is de achtste  paragraaf</p>
+<p>Dit is de negende paragraaf</p>
+<p>Dit is de tiende paragraaf</p>
--- tutorials/jquery/tut_jquery_syntax.php
+++ tutorials/jquery/tut_jquery_syntax.php
@@ -9,15 +9,44 @@
 		// Javascript werkt gewoon!
 		document.getElementById("demoName").innerHTML = "Mijn naam is: " + name;
 		
+		//$("#btn_1").css("border", "2px solid grey");
+		//$("#btn_1").css("padding", "20px");
+		
 		$("#btn_1").click(function()
 		{
 			$("p").hide();
+			$(this).text("DubbelKlik op mij!");
+			///var padding = $(this).css("padding");
+			//alert(padding);
+			$(this).css("padding", "20px")
+				   .css("border", "20px solid grey")
+				   .css("background-color", "rgba(20, 200, 20, 0.5)")
+				   .css("font-size", "2em")
+				   .css("font-weight", "bold")
+				   .css("color", "white");
+			
 		});	
-
+		
+		$("#btn_1").dblclick(function()
+		{
+			$("p").show();
+			$(this).text("Klik op mij!");
+						
+			$(this).css( {"padding" 		: "30px",
+						  "backgroundColor" : "purple",
+						  "font-size"		: "4em",
+						  "float"			: "right"} );
+			var padding = $("#btn_1").css("padding");
+			console.log("De padding is: " + padding);
+		});
+		
+		
 		$("#btn_2").dblclick(function()
 		{
 			$("li").hide();
 		});
+			
+		
 	});
 	
 	
@@ -31,7 +60,7 @@
 <p>Dit is de vierde</p>
 <p>Dit is de vijfde</p>
 <p>Dit is de zesde</p>
-<button id="btn_1">Verdwijn!</button> 
+<button id="btn_1">Klik op mij!</button> 
 
 <ul>
 	<li>eerste</li>
