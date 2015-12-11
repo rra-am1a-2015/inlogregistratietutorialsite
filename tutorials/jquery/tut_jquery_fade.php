398019de82c51450dc05099a1d1b7a52051b31fe
Arjan de Ruijter <rra@mboutrecht.nl>
2015-12-11 10:35:59 +0100

    fadetoggle

--- tutorials/jquery/tut_jquery_fade.php
+++ tutorials/jquery/tut_jquery_fade.php
@@ -2,14 +2,13 @@
 <h4>Fade example</h4>
 <button id="btn_fade_in">Fade in</button>
 <button id="btn_fade_out">Fade out</button>
+<button id="btn_fade_toggle">Fade toggle out</button>
 <div id="fade_example">
 klik op deze zin en niet op de knop voor css opmaak
 </div>
 
 
 <script>
-	var schoenmaat = 44;
-	
 	var btnCSS = { style : "padding: 1em;" +
 						   "border-radius: 0.5em;" +
 						   "border: 0px solid red;" + 
@@ -31,6 +30,22 @@ klik op deze zin en niet op de knop voor css opmaak
 							$("#fade_example").fadeIn(2000, function(){
 								console.log("Ik ben er weer!!");
 							})
+						
+						});
+						
+		$("#btn_fade_toggle").attr(btnCSS)
+						.click(function(){
+							$("#fade_example").fadeToggle(2000, function(){
+								console.log("Ik ben er weer!!" + $(this).text());
+								if ( $("#btn_fade_toggle").text() == "Fade toggle out")
+								{
+									$("#btn_fade_toggle").text("Fade toggle in");
+								}
+								else
+								{
+									$("#btn_fade_toggle").text("Fade toggle out");
+								}
+							});
 						
 						});
 		
