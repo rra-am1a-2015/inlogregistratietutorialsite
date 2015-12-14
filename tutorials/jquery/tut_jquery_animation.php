9316852a10d882ca3f7c990c42372d8faaa27059
Arjan de Ruijter <rra@mboutrecht.nl>
2015-12-14 09:52:28 +0100

    animation

--- tutorials/jquery/tut_jquery_animation.php
+++ tutorials/jquery/tut_jquery_animation.php
@@ -5,9 +5,24 @@
 <button id="btn_animation">Start animatie</button>
 
 <script>
-	$("#btn_animation").hover(function(){
-		$(this).css("box-shadow", "10px 10px 5px #888888");
-	}, function(){
-		$(this).css("box-shadow", "0px 0px 0px #888888");
+	$("document").ready(function(){
+	
+	// Toevoegen schaduw wanneer er over de button wordt gehoverd.
+		$("#btn_animation").hover(function(){
+			$(this).css("box-shadow", "10px 10px 5px #888888");
+		}, function(){
+			$(this).css("box-shadow", "0px 0px 0px #888888");
+		});
+
+	
+	// De animation....
+		var changeCSS = { left : "200px"};
+	
+		$("#btn_animation").click(function(){
+			//alert("Hallo!");
+			$("#animation_demo").animate(changeCSS, 10000, function(){ alert("He He ik ben er!");});
+		
+		});
 	});
+	
 </script>
