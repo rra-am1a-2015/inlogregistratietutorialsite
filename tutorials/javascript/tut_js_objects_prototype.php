0d2d7c289c8ba28c8b6e65ce64a3c8b51404f1f2
Arjan de Ruijter <rra@mboutrecht.nl>
2016-02-11 15:28:46 +0100

    prototype reviews

--- tutorials/javascript/tut_js_objects_prototype.php
+++ tutorials/javascript/tut_js_objects_prototype.php
@@ -5,26 +5,51 @@
 <script>
 
 // Dit is een prototype function voor javascript objecten van het type game.
-function game(name, genre, price)
+function game(name, genre, price, rating)
 {
 	this.name = name;
 	this.genre = genre;
-	this.price = price;	
+	this.price = price;
+	this.rating = rating;
+	this.hallo = function()
+	{
+		alert("Hallo");
+	}
+	this.reviews = [this.name, "Beste game ooit!", "Super schamend slecht!!"];
+	this.showReviews = function(){
+		var txt = "";
+		for (var i = 0; i < this.reviews.length; i++)
+		{
+			txt += this.reviews[i] + "<br>";
+		}
+		return txt;
+	}
 }
 
+game.prototype.showInfo = function()
+{
+	//alert("doei");
+	return		"Naam game: " + this.name + "<br>" +
+			    "Genre game: " + this.genre + "<br>" +
+				"Rating game: " + this.rating + " sterren<br>" +
+				"Reviews van: " + this.showReviews() + 
+			    "Prijs game: &euro; " + this.price + "<hr>";	
+}
 
-var eersteGame = new game("Kingsvalley I", "platformer", 0.85);
-var tweedeGame = new game("Pyramid Panic", "doolhofgame", 0.00);
-var derdeGame = new game("Tomb Raider I", "adventuregame", 21.99);
-var vierdeGame = new game("Frogger", "arcadegame", 1.00);
+//game.prototype.rating = 4;
+
+
+var eersteGame = new game("Kingsvalley I", "platformer", 0.85, 3);
+var tweedeGame = new game("Pyramid Panic", "doolhofgame", 0.00, 5);
+var derdeGame = new game("Tomb Raider I", "adventuregame", 21.99, 5);
+var vierdeGame = new game("Frogger", "arcadegame", 1.00, 2);
 
 var games = [eersteGame, tweedeGame, derdeGame, vierdeGame];
 
 var output = "";
 for ( var i = 0; i < games.length; i++)
 {
-	output += "Naam game: " + games[i].name + "<br>";
-	
+	output += games[i].showInfo();	
 }
 
 var pProtoTxt = document.getElementById("p-prototype");
