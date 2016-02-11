5ed34a40f44341e0c45c23c8b30cf329756d72de
Arjan de Ruijter <rra@mboutrecht.nl>
2016-02-11 13:23:18 +0100

    reddingsactie

--- topic_links/javascript_link.php
+++ topic_links/javascript_link.php
@@ -4,3 +4,4 @@
 <a href='index.php?content=developer_homepage&page=tutorials/javascript/tut_js_for_loops&topic=javascript'>JS for loops</a>
 <a href='index.php?content=developer_homepage&page=tutorials/javascript/tut_js_functions&topic=javascript'>JS functions</a>
 <a href='index.php?content=developer_homepage&page=tutorials/javascript/tut_js_arrays&topic=javascript'>JS arrays</a>
+<a href='index.php?content=developer_homepage&page=tutorials/javascript/tut_js_objects&topic=javascript'>JS objects</a>
---
+++ tutorials/javascript/tut_js_objects.php
@@ -0,0 +1,37 @@
+<h1>In JavaScript, objects are king. If you understand objects, you understand JavaScript.</h1>
+
+<p id="showObjectValue"></p>
+
+<script>
+	// Dit is een javascriptobject
+	var persoon = { voornaam			: "Arjan",
+					tussenvoegsel		: "de",
+					achternaam			: "Ruijter",
+					leeftijd			: 47,
+					oogkleur			: "zwart",
+					mijnEersteJSMethod	: function() {
+											alert("Hallo JS Object Wereld!");										  
+										  },
+					toonPersoonsgegevens : function() {
+											return	"Voornaam: " + this.voornaam + "<br>" +
+													"tussenvoegsel: " + this.tussenvoegsel + "<br>" +
+													"achternaam: " + this.achternaam + "<br>" +
+													"leeftijd: " + this.leeftijd + "<br>";
+										  },
+					lievelingsKleuren	: ["geel", "groen", "blauw"]
+				  };
+				  
+				  
+	
+	var pTag = document.getElementById("showObjectValue");
+	
+	//pTag.innerHTML = persoon.toonPersoonsgegevens();
+	
+	pTag.innerHTML = persoon.lievelingsKleuren[0];
+	
+	
+	
+	
+
+
+</script>
